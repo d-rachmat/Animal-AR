dba2ea2e5b7d0da38b03e973a1031248daa3d369
drachmat <drachmat.iskandar@gmail.com>
2018-11-02 07:24:07 +0700

    innitial commit

---
+++ Assets/Script/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject menu;
+	
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3);
+        menu.SetActive(true);
+    }
+
+    public void StartButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("AR"); 
+    }
+
+    public void CloseButton()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/SwipeToRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeToRotate : MonoBehaviour {
+
+    float rotSpeed = 100;
+    float rotSpeeds = 60;
+    void OnMouseDrag()
+    {
+        float rotY = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        transform.Rotate(0, -rotY, 0, Space.Self);
+
+        float rotX = Input.GetAxis("Mouse Y") * rotSpeeds * Mathf.Deg2Rad;
+        transform.Rotate(rotX, 0, 0, Space.Self);
+    }
+}
